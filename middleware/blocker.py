71da772998d725007d9d7eab58f300bf88e04a4d
salexkidd <salexkidd@gmail.com>
2012-03-06 22:45:00 +0900

    modify static url and cache time
    And comment, remove logging information

--- middleware/blocker.py
+++ middleware/blocker.py
@@ -4,28 +4,28 @@ from django.core.cache import cache
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse as url_reverse
 
-import logging
-logger = logging.getLogger('console.debug')
+
+REDICECT_URL = "http://www.google.com"
+CACHE_TIME = 7200
 
 
 class DoublePostBlockerMiddleware:
     """
-    POSTにて一緒に投げられたCSRFを一時的にキャッシュし、同じCSRFキーをもったPOSTがきたら無視する
+    POSTにて一緒に投げられた{% dpb_token %}(生成されたmd5値)を一時的にキャッシュし、
+    同じdbp_tokenキーをもったPOSTがきたら無視する
 
     このmiddlewareを利用する場合は
     1.django.middleware.csrf.CsrfViewMiddlewareを組み込んでください
-    2.POSTを行う場合は {% csrf_token %} をテンプレートに記述してください
+    2.POSTを行う場合は {% dbp_token %} をテンプレートに記述してください
     """
     def process_request(self, request):
         if request.method == "POST":
             if "dpb_token" in request.POST:
                 if cache.get(request.POST['dpb_token']):
                     url = url_reverse('list_diary')
-                    logger.debug("Cache has same csrf token. double post")
-                    return HttpResponseRedirect(url)
+                    return HttpResponseRedirect(REDICECT_URL)
                 else:
                     cache_key = request.POST['dpb_token']
-                    cache.set(cache_key, 1, 7200)
+                    cache.set(cache_key, 1, CACHE_TIME)
             else:
-                logger.debug("dpb_token not found.")
         return None
